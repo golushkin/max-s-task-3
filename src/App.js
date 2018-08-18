475f197c95cc21ae2f8045a6fdaee63cb2e5aafa
golushkin <andrewpb@bk.ru>
2018-08-18 10:41:43 +0300

    start pack

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class App extends React.Component{
+    render(){
+        return (
+            <div className='wrapper'>
+                <h1>hi</h1>
+            </div>
+        )
+    }
+}
+
